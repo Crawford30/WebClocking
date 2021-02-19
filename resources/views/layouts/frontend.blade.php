1bca05cf6285bf1865b845326c5ecf9d64f89f39
joel <joelcrawford30@gmail.com>
2021-02-19 14:06:25 +0300

    Navbar brand

---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title') | Web Clocking</title>
+
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+
+    <link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    />
+    <link
+    href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,700;1,700&display=swap"
+    rel="stylesheet"
+    />
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" > 
+   <!--  <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/animate.css" /> -->
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+</head>
+
+
+
+<body class="body">
+
+
+    <!-- Header/nav -->
+    <div class="custom-navbar">
+         @include('layouts.includes.front-navbar')
+    </div>
+   
+
+
+    <!-- Main content -->
+    <main>
+        @yield('content')
+    </main>
+
+
+    <!-- footer -->
+    @include('layouts.includes.front-footer')
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ resources/views/layouts/includes/front-navbar.blade.php
@@ -0,0 +1,38 @@
+<div id="app" class="container-fliud">
+  <nav class="navbar navbar-expand-md navbar-light">
+    <a class="navbar-brand" href="#" style="color: #fff;">Clocking <small style="display:block;
+    font-size:12px; color: #fff;">joel@gmail.com(Admin)</small></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mr-auto">
+               <!--  <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  </li> -->
+                <!-- <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                  </li> -->
+
+                </ul>
+
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/report') }}" style="color: #fff;">Report</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/user') }}" style="color: #fff;">User</a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/logout') }}" style="color: #fff;">Logout</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+
+            <!-- <div style="display: flex; direction: column;"> -->
+             <!--  <h6>joel@gmail.com(Admin)</h6> -->
+             <!--  </div> -->
+           </div>
+
